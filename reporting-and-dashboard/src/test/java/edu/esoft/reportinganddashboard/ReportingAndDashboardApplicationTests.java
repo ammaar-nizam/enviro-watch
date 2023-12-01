bb0451864f96f7ce57c3ec61b16d3f93e6e97f3f
ammaar-nizam <ammaar.nizam@outlook.com>
2023-12-01 21:51:07 +0530

    Addign reporting-and-dashboard

---
+++ reporting-and-dashboard/src/test/java/edu/esoft/reportinganddashboard/ReportingAndDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esoft.reportinganddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingAndDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
