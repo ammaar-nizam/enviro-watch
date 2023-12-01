267e466fcdda20291bd3f2bfdeafd55fc73601ef
ammaar-nizam <ammaar.nizam@outlook.com>
2023-12-01 21:50:25 +0530

    Adding notification-management

---
+++ notification-management/src/main/java/edu/esoft/notificationmanagement/NotificationManagementApplication.java
@@ -0,0 +1,13 @@
+package edu.esoft.notificationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationManagementApplication.class, args);
+	}
+
+}
---
+++ notification-management/src/test/java/edu/esoft/notificationmanagement/NotificationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.esoft.notificationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
